5480c413edc4568acb2c74894ad0b6a8ef0133a6
Austin Kurpuis <austin@bitovi.com>
2023-06-07 16:17:42 -0700

    Enable terminal flag

--- src/repl.js
+++ src/repl.js
@@ -3,10 +3,10 @@ import { getSolutionStream } from './openai.js'
 
 export const GPT_COMMAND = 'gpt'
 
-export async function gpt (replServer, problem) {
+export async function gpt(replServer, problem) {
   const solutionStream = await getSolutionStream(problem)
   await new Promise(resolve => {
-    function handleData (data) {
+    function handleData(data) {
       replServer.write(data.toString('utf8'))
     }
     solutionStream.on('data', handleData)
@@ -17,17 +17,17 @@ export async function gpt (replServer, problem) {
   })
 }
 
-export function createReplServer (input = process.stdin, output = process.stdout) {
+export function createReplServer(input = process.stdin, output = process.stdout) {
   const replServer = repl.start({
     input,
     output,
-    // terminal: true,
+    terminal: true,
     preview: true
   })
 
   replServer.defineCommand(GPT_COMMAND, {
     help: 'Use GPT to generate code directly in the REPL session.',
-    action (problem) {
+    action(problem) {
       this.clearBufferedCommand()
       gpt(this, problem).then(() => this.displayPrompt())
     }
