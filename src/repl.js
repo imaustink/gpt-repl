ba4eab00ec9da128e0fd8fa1377703fed6689f2e
Austin Kurpuis <austin@bitovi.com>
2023-06-07 16:40:45 -0700

    Fix build and add flags to enable terminal and preview

--- src/repl.js
+++ src/repl.js
@@ -1,12 +1,14 @@
 import repl from 'node:repl'
 import { getSolutionStream } from './openai.js'
 
+const [/* binPath */, /* mainPath */, ...args] = process.argv
+
 export const GPT_COMMAND = 'gpt'
 
-export async function gpt(replServer, problem) {
+export async function gpt (replServer, problem) {
   const solutionStream = await getSolutionStream(problem)
   await new Promise(resolve => {
-    function handleData(data) {
+    function handleData (data) {
       replServer.write(data.toString('utf8'))
     }
     solutionStream.on('data', handleData)
@@ -17,17 +19,24 @@ export async function gpt(replServer, problem) {
   })
 }
 
-export function createReplServer(input = process.stdin, output = process.stdout) {
+export function createReplServer (input = process.stdin, output = process.stdout) {
   const replServer = repl.start({
     input,
     output,
-    terminal: true,
-    preview: true
+    ...(args.includes('--forceTerminal') && { terminal: true }),
+    ...(args.includes('--forcePreview') && { preview: true })
+  })
+
+  console.log({
+    input,
+    output,
+    ...(args.includes('--forceTerminal') && { terminal: true }),
+    ...(args.includes('--forcePreview') && { preview: true })
   })
 
   replServer.defineCommand(GPT_COMMAND, {
     help: 'Use GPT to generate code directly in the REPL session.',
-    action(problem) {
+    action (problem) {
       this.clearBufferedCommand()
       gpt(this, problem).then(() => this.displayPrompt())
     }
